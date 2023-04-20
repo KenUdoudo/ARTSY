a9e5415b881c0b9259448b7c588e209b4ee50cbb
Ken <kendo302003@gmail.com>
2023-04-20 19:16:29 +0100

    omorh

--- src/Home/NavBar.jsx
+++ src/Home/NavBar.jsx
@@ -1,22 +1,17 @@
-import Hamburger from './src/assets/home assets/Frame 38hamburger.png'
+import Hamburger from './assets/Frame 38hamburger.png'
+
 const NavBar = () => {
 
-  const [open, setOpen] = useState(false);
+
 
   return (
-    <div className="navbar">
+    <div className="NavBar">
       <div>
-      <img src={ Hamburger } onClick={()=>{setOpen(!open)}} id='ham'/>
+      <img src={ Hamburger }/>
 
-      <div className={`navlinks ${open? 'active' : 'inactive'}`}>
-        <ul className="menu">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Auctions</a></li>
-          <li><a href="#">Marketplace</a></li>
-          
-        </ul>
-      </div>
+
       
+      <h3>ARTSY</h3>
     </div>
       
     </div>
