2f0d28c1e5aa56b2866ff98cdb412f80d98518d4
Ken <kendo302003@gmail.com>
2023-04-24 17:21:17 +0100

    creating new assets folder

--- src/Home/Section1.jsx
+++ src/Home/Section1.jsx
@@ -1,8 +1,16 @@
+import CarouselOne from './src/assets/Home/Homepage carousel WEB.png'
+
 const Section1home = () => {
   return (
 
     <div className="sec">
       <h2>Photography is poetry & <br/>beatiful untold stories</h2>
+
+      <p>Flip through more than 10,000 vintage shots, old photographs, historic images and captures seamlessly in one place. Register to get top access</p>
+
+      <div>
+        <img src={ CarouselOne }/>
+      </div>
     </div>
 
   );
