e39b7b06992861c47baea56133cacf9680e04888
Ken <kendo302003@gmail.com>
2023-04-24 17:28:17 +0100

    br

--- src/Home/Section1.jsx
+++ src/Home/Section1.jsx
@@ -9,7 +9,7 @@ const Section1home = () => {
     <div className="sec">
       <h2>Photography is poetry & <br/>beatiful untold stories</h2>
 
-      <p>Flip through more than 10,000 vintage shots, old photographs, historic images and captures seamlessly in one place. Register to get top access</p>
+      <p>Flip through more than 10,000 vintage shots, old photographs, historic <br />images and captures seamlessly in one place. Register to get top access</p>
 
       <div>
         <img src={ CarouselOne }/>
