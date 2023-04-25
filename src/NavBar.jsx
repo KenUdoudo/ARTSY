0eed9b9725c434dce02384f71f4f2df16e6a9672
Ken <kendo302003@gmail.com>
2023-04-25 13:08:24 +0100

    styling navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,18 +6,19 @@ const NavBar = () => {
 
   return (
     <div className="NavBar">
-      <div>
+      <div className="flex flex-row justify-around">
         <h3>ARTSY</h3>
 
-        <ul>
+        <ul className="flex flex-row">
           <li><a href="#">Home</a></li>
           <li><a href="#">Marketplace</a></li>
           <li><a href="#">Auctions</a></li>
           <li><a href="#">Drop</a></li>
         </ul>
-
+       <div>
         <img src={ Cart }/>
         <img src= { Bell }/>
+        </div>
       </div>
       
     </div>
