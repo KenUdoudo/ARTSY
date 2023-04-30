be280bfd803cfd21a2bb55618e8b9330dff13c80
Ken <kendo302003@gmail.com>
2023-04-30 14:28:59 +0100

    hr css

--- src/Home/Section2.jsx
+++ src/Home/Section2.jsx
@@ -2,6 +2,14 @@ const Section2home = () => {
   return (
     <div className="mx-20">
       <h3 className="mt-14 text-2xl text-gray-700">Featured products</h3>
+
+      <div>
+        <hr className="my-6"/>
+
+
+      </div>
+
+
     </div>
   );
 }
