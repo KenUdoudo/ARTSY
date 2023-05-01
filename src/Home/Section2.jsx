6ea7450404cc389be8649491a008d960ceadc170
Ken <kendo302003@gmail.com>
2023-05-01 09:11:38 +0100

    resizing arrow

--- src/Home/Section2.jsx
+++ src/Home/Section2.jsx
@@ -13,9 +13,9 @@ const Section2home = () => {
           <div className='flex flex-col gap-y-6'>
             <h3 className='text-xl font-bold'>The Boolean Egyptian</h3>
             <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ad deleniti delectus cum at harum ipsum fugiat laboriosam culpa vitae, sed cumque iure corrupti exercitationem enim odit. Explicabo, voluptates praesentium!</p>
-            <div>
-              <img src={ groupProfile }/>
-              <img src={ arrow }/>
+            <div className='flex flex-row justify-between items-center'>
+              <img src={ groupProfile } className='w-auto h-2/4'/>
+              <img src={ arrow } className='w-10 h-10'/>
             </div>
           </div>
         </div>
