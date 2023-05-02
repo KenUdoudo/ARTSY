891e2d74eb17cc0ad01569d335575f5553e2dac4
Ken <kendo302003@gmail.com>
2023-05-02 08:00:23 +0100

    adding z index

--- src/Home/Section3.jsx
+++ src/Home/Section3.jsx
@@ -2,7 +2,7 @@ import backdrop from "./assets/Rectangle 90web gradient.png"
 const Section3home = () => {
   return (
     <div className="mt-40">
-      <img src={ backdrop } className="w-full h-auto"/>
+      <img src={ backdrop } className="w-full h-auto -z-10"/>
     </div>
   );
 }
