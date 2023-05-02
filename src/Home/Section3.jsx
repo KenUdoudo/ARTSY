19f851109fd5f1e4f09551f24a35133e64b81780
Ken <kendo302003@gmail.com>
2023-05-02 11:54:21 +0100

    importing img inside gradient

--- src/Home/Section3.jsx
+++ src/Home/Section3.jsx
@@ -1,10 +1,14 @@
 import backdrop from "./assets/Rectangle 90web gradient.png"
+import inside from './assets/Rectangle 91inside gradient.png'
 const Section3home = () => {
   return (
     <div className="static mt-40">
       <div className="absolute mx-20 z-10">
         <h3 className="mt-6 text-2xl text-white">See Upcoming Auctions and Exibitions</h3>
         <hr className="my-4 mx-2" />
+        <div>
+          <img src={ inside }/>
+        </div>
       </div>
       <img src={ backdrop } className="w-full h-auto z-0"/>
       
