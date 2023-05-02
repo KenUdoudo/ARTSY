e0f5a4a16c9755093719cbc81f0097390ff8122a
Ken <kendo302003@gmail.com>
2023-05-02 11:49:07 +0100

    sec3header

--- src/Home/Section3.jsx
+++ src/Home/Section3.jsx
@@ -3,7 +3,8 @@ const Section3home = () => {
   return (
     <div className="static mt-40">
       <div className="absolute mx-20 z-10">
-        <h3>See Upcoming Auctions and Exibitions</h3>
+        <h3 className="mt-6 text-2xl text-white">See Upcoming Auctions and Exibitions</h3>
+        <hr className="my-4 mx-2" />
       </div>
       <img src={ backdrop } className="w-full h-auto z-0"/>
       
