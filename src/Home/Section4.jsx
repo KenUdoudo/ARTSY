c0814ebca6455cd625d90b22c1eba098b9d31097
Ken <kendo302003@gmail.com>
2023-05-05 10:15:35 +0100

    importing loader

--- src/Home/Section4.jsx
+++ src/Home/Section4.jsx
@@ -1,7 +1,13 @@
+import loader2 from './assets/Loader (1).png'
 const Section4home = () => {
   return (
     <div className="bg-gray-200 mt-20">
-
+      <div>
+        <h2>TOP CREATORS OF <br />THE WEEK</h2>
+        <div>
+          <img src={ loader2 }/>
+        </div>
+      </div>
     </div>
   );
 }
