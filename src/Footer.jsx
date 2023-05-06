701bd770e1060a03f74ce0c37c27a1ed8d51b6e5
Ken <kendo302003@gmail.com>
2023-05-06 20:06:20 +0100

    newsletter write up

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <h3>NEWSLETTER</h3>
+        <p>Subscribe to get daily updates on news drops & exciting deals</p>
+      </div>
+    </div>
+  );
+}
+ 
+export default Footer;
