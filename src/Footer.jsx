1c801a59f7896f2df47da3a28a80906d688639db
Ken <kendo302003@gmail.com>
2023-05-07 12:23:08 +0100

    form

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
         <h3>NEWSLETTER</h3>
         <p>Subscribe to get daily updates on news drops & exciting deals</p>
         <div>
-          
+          <form>
+            <input type="email" placeholder="ENTER YOUR EMAIL"/>
+            <button type="submit">SUBSCRIBE</button>
+          </form>
         </div>
       </div>
     </div>
