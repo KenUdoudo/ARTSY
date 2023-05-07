2254706a07d39dd4f250146fa59b50a2647388bb
Ken <kendo302003@gmail.com>
2023-05-07 12:39:00 +0100

    border around

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <div className="mt-40">
-      <div className="flex flex-col justify-center items-center gap-y-4">
-        <h3>NEWSLETTER</h3>
-        <p>Subscribe to get daily updates on news drops & exciting deals</p>
+      <div className="flex flex-col justify-center items-center gap-y-4 border-2 mx-10 py-10">
+        <h3 className="text-lg">NEWSLETTER</h3>
+        <p className="text-lg">Subscribe to get daily updates on news drops & exciting deals</p>
         <div>
           <form>
             <input type="email" placeholder="ENTER YOUR EMAIL" className="border-2 border-gray-500 pl-5 pr-16 py-1 text-sm"/>
