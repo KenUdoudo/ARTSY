26b7f347d5f1a98d84e78f410f56d8572ea0b206
Ken <kendo302003@gmail.com>
2023-05-07 12:40:41 +0100

    done with news letter

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <h3 className="text-lg">NEWSLETTER</h3>
         <p className="text-lg">Subscribe to get daily updates on news drops & exciting deals</p>
         <div>
-          <form>
+          <form className="flex flex-row gap-x-4  ">
             <input type="email" placeholder="ENTER YOUR EMAIL" className="border-2 border-gray-500 pl-5 pr-16 py-1 text-sm"/>
             <button type="submit" className="text-sm text-white bg-black px-2 py-1 border-2 border-black">SUBSCRIBE</button>
           </form>
