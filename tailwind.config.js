ef7e13d7f56cce99aeff485a2b85acab78e2fe86
Ken <kendo302003@gmail.com>
2023-05-07 14:06:33 +0100

    importing font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      stix: ['STIX Two Text', 'serif']
+      satoshi: ['Raleway', 'sans-serif']
+      clash: ['Bruno Ace SC', 'cursive']
+    }
   },
   plugins: [],
 }
