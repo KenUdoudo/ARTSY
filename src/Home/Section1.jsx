c6a2fde0c735171f42a50c4d35d93f7a087fa8ba
Ken <kendo302003@gmail.com>
2023-05-09 17:47:26 +0100

    trying responsiveness

--- src/Home/Section1.jsx
+++ src/Home/Section1.jsx
@@ -7,15 +7,15 @@ const Section1home = () => {
   return (
 
     <div className="flex flex-col justify-center items-center gap-y-5 text-center mt-5">
-      <h2 className='text-6xl font-bold font-bruno'>Photography is poetry & <br/>beautiful untold stories</h2>
+      <h2 className='text-6xl lg:text-5xl font-bold font-bruno'>Photography is poetry & <br/>beautiful untold stories</h2>
 
       <p className='text-lg font-rale'>Flip through more than 10,000 vintage shots, old photographs, historic <br />images and captures seamlessly in one place. Register to get top access.</p>
 
       <div className='flex flex-row justify-between gap-x-2 w-full overflow-hidden'>
-        <img src={ CarouselOne } className='w-4/5'/>
-        <img src={ CarouselTwo } className= 'w-4/5'/>
-        <img src={ CarouselThree } className='w-4/5'/>
-        <img src={ CarouselFour } className='w-4/5'/>
+        <img src={ CarouselOne } className='w-4/5 lg:w-3/5 lg:h-40'/>
+        <img src={ CarouselTwo } className= 'w-4/5 lg:w-3/5 lg:h-40'/>
+        <img src={ CarouselThree } className='w-4/5 lg:w-3/5 lg:h-40'/>
+        <img src={ CarouselFour } className='w-4/5 lg:w-3/5 lg:h-40'/>
       </div>
     </div>
 
