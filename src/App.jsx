92a8b91c725a7871eb70f7899b31fabf884b0052
Ken <kendo302003@gmail.com>
2023-05-11 09:31:11 +0100

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   
   return (
     <Router>
+      <Routes>
     <div className="App">
       
 
@@ -13,6 +14,7 @@ function App() {
     <Home />
 
     </div>
+    </Routes>
     </Router>
   )
 }
