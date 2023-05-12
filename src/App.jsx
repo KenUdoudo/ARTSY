c6d3859a0c4daeb3a07dfb6fff930b64e06e3c4f
Ken <kendo302003@gmail.com>
2023-05-12 12:44:41 +0100

    trying out new routing method

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
-import Home from './Home'
 import Drops from './Drops'
+import Home from './Home'
+
 function App() {
   
   return (
     
     <div className="App">
-      <Router>
+     
+     
       <Routes>
-        <Route exact path="/" element={ <Home /> } />
-        <Route exact path="/Drops" element={ <Drops /> } />
+      
+      <Route path="/" element={ <Home /> } />
+      <Route path="/Drops" element={ <Drops /> } />
 
 
-    </Routes>
-    </Router>
+    
 
+
+     </Routes>
+  
     </div>
     
   )
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,9 @@ const NavBar = () => {
 
 
   return (
+    
     <div className="NavBar">
+      
       <div className="flex flex-row justify-between px-20 mt-5 items-center">
         <h3 className="text-2xl font-bold font-stix">ARTSY.</h3>
 
@@ -14,7 +16,7 @@ const NavBar = () => {
           <li><a href="#">Home</a></li>
           <li><a href="#">Marketplace</a></li>
           <li><a href="#">Auctions</a></li>
-          <li><a href='./Drops'>Drop</a></li>
+          <li><a href='#'>Drop</a></li>
         </ul>
        <div className="flex flex-row gap-x-5 items-center">
         <img src={ search } className="w-10 h-10"/>
