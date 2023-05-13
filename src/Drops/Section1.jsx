19ad8b96611e0454bb32a0a7a2c7e0dc455ef524
Ken <kendo302003@gmail.com>
2023-05-13 09:20:10 +0100

    drops sec1

--- src/Drops/Section1.jsx
+++ src/Drops/Section1.jsx
@@ -2,6 +2,8 @@ const Section1drops = () => {
   return (  
     <div>
       <h3>Upcoming Drops</h3>
+      <p>Turn on notifications so that no drops will miss you</p>
+      <button>Notify me</button>
     </div>
   );
 }
