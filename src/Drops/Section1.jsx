24ca14e2cd5703a328fb245d5fd7f3e751189598
Ken <kendo302003@gmail.com>
2023-05-13 09:28:06 +0100

    main sec1 css

--- src/Drops/Section1.jsx
+++ src/Drops/Section1.jsx
@@ -1,6 +1,6 @@
 const Section1drops = () => {
   return (  
-    <div>
+    <div className="flex flex-col mt-8 mx-20">
       <h3>Upcoming Drops</h3>
       <p>Turn on notifications so that no drops will miss you</p>
       <button>Notify me</button>
