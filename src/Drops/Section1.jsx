ee049ee01af6fdbd542795fbe7381dbeb271949c
Ken <kendo302003@gmail.com>
2023-05-13 19:58:44 +0100

    done with sec1

--- src/Drops/Section1.jsx
+++ src/Drops/Section1.jsx
@@ -3,7 +3,7 @@ const Section1drops = () => {
     <div className="flex flex-col mt-8 mx-20 items-center">
       <h3 className="text-center text-xl">Upcoming Drops</h3>
       <p className="text-center mt-4 text-lg">Turn on notifications so that no drops will miss you</p>
-      <button className="mt-4 border rounded px-12 py-1">Notify me</button>
+      <button className="mt-4 border border-black rounded px-12 py-1">Notify me</button>
     </div>
   );
 }
