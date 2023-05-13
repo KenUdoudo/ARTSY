430be5dd295a5586148b3350fe3c005da7083608
Ken <kendo302003@gmail.com>
2023-05-13 19:57:36 +0100

    notify me positioning

--- src/Drops/Section1.jsx
+++ src/Drops/Section1.jsx
@@ -1,9 +1,9 @@
 const Section1drops = () => {
   return (  
-    <div className="flex flex-col mt-8 mx-20">
+    <div className="flex flex-col mt-8 mx-20 items-center">
       <h3 className="text-center text-xl">Upcoming Drops</h3>
       <p className="text-center mt-4 text-lg">Turn on notifications so that no drops will miss you</p>
-      <button>Notify me</button>
+      <button className="mt-4 border rounded px-12 py-1">Notify me</button>
     </div>
   );
 }
