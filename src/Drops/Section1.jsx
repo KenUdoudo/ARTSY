cf638b32f42436ebff4dea6d599bb07ff1df3fef
Ken <kendo302003@gmail.com>
2023-05-13 09:30:07 +0100

    position and size

--- src/Drops/Section1.jsx
+++ src/Drops/Section1.jsx
@@ -1,8 +1,8 @@
 const Section1drops = () => {
   return (  
     <div className="flex flex-col mt-8 mx-20">
-      <h3>Upcoming Drops</h3>
-      <p>Turn on notifications so that no drops will miss you</p>
+      <h3 className="text-center text-xl">Upcoming Drops</h3>
+      <p className="text-center mt-4 text-lg">Turn on notifications so that no drops will miss you</p>
       <button>Notify me</button>
     </div>
   );
