8cda8d8791762a581ef9ee3302549cd1018a6b5c
Ken <kendo302003@gmail.com>
2023-05-13 09:21:03 +0100

    importing navbar to drops

--- src/Drops.jsx
+++ src/Drops.jsx
@@ -1,9 +1,12 @@
 import Section1drops from "./Drops/Section1";
+import NavBar from "./NavBar";
 
 const Drops = () => {
   return (
     <div>
 
+      <NavBar />
+      
       <Section1drops />
 
     </div>
