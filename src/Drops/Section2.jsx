11f70665716c205bd90362fb4590934c9d6611b9
Ken <kendo302003@gmail.com>
2023-05-14 08:01:12 +0100

    importing img

--- src/Drops/Section2.jsx
+++ src/Drops/Section2.jsx
@@ -1,6 +1,16 @@
+import eyo from './assets/Rectangle 82.png'
+
 const Section2drops = () => {
   return (
     <div className="mx-20">
+      <div>
+        <div><img src={ eyo }/></div>
+      </div>
+
+
+
+
+
 
     </div>
   );
