304d7217e68c193eb62eeb3a47719939db905fb1
Ken <kendo302003@gmail.com>
2023-05-14 08:07:01 +0100

    done with text

--- src/Drops/Section2.jsx
+++ src/Drops/Section2.jsx
@@ -11,6 +11,9 @@ const Section2drops = () => {
           <p>UPCOMING</p>
           <p>November 21 at 11 am WAT</p>
           <h3>Eyo : Eko For Show</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi reiciendis, voluptatem dolore voluptate eum voluptatum possimus libero? Laudantium, ratione distinctio nulla soluta perferendis iure voluptate eaque ab aut. Deleniti, quod.</p>
+          <p>Creator : <a href="#"><span>Aliya Minat</span></a></p>
+          <a href="#">Get notified</a>
         </div>
       </div>
 
