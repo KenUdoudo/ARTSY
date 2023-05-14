2d8656612707bb0e0d6d13b3f093be85d663c8ff
Ken <kendo302003@gmail.com>
2023-05-14 07:52:36 +0100

    font weight

--- src/Drops/Section1.jsx
+++ src/Drops/Section1.jsx
@@ -1,9 +1,9 @@
 const Section1drops = () => {
   return (  
     <div className="flex flex-col mt-8 mx-20 items-center">
-      <h3 className="text-center text-xl">Upcoming Drops</h3>
+      <h3 className="text-center text-xl font-semibold">Upcoming Drops</h3>
       <p className="text-center mt-4 text-lg">Turn on notifications so that no drops will miss you</p>
-      <button className="mt-4 border border-black rounded px-12 py-1">Notify me</button>
+      <button className="mt-4 border border-black rounded px-12 py-1 font-semibold">Notify me</button>
     </div>
   );
 }
