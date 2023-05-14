781c5d07e96d76754b4a04d86918d95c56254ab6
Ken <kendo302003@gmail.com>
2023-05-14 08:08:57 +0100

    flex

--- src/Drops/Section2.jsx
+++ src/Drops/Section2.jsx
@@ -3,7 +3,7 @@ import eyo from './assets/Rectangle 82.png'
 const Section2drops = () => {
   return (
     <div className="mx-20">
-      <div>
+      <div className='flex flex-row'>
         <div>
           <img src={ eyo }/>
         </div>
@@ -11,7 +11,7 @@ const Section2drops = () => {
           <p>UPCOMING</p>
           <p>November 21 at 11 am WAT</p>
           <h3>Eyo : Eko For Show</h3>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi reiciendis, voluptatem dolore voluptate eum voluptatum possimus libero? Laudantium, ratione distinctio nulla soluta perferendis iure voluptate eaque ab aut. Deleniti, quod.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi <br /> reiciendis, voluptatem dolore voluptate eum voluptatum possimus libero? </p>
           <p>Creator : <a href="#"><span>Aliya Minat</span></a></p>
           <a href="#">Get notified</a>
         </div>
