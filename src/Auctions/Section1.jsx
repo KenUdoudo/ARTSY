7c170c291ee235152f08bf7f63f7a4fcabdd1467
Ken <kendo302003@gmail.com>
2023-05-16 07:46:28 +0100

    auction p styling

--- src/Auctions/Section1.jsx
+++ src/Auctions/Section1.jsx
@@ -1,7 +1,7 @@
 const Section1auction = () => {
   return (  
     <div className="mx-20">
-      <p>Here's an overview of products actively on auction, explore!</p>
+      <p className="mt-28 font-semibold">Here's an overview of products actively on auction, explore!</p>
     </div>
   );
 }
