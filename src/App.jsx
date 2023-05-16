8b91aa9810f3ac77d47f1b8680f72328786d60bb
Ken <kendo302003@gmail.com>
2023-05-16 07:33:56 +0100

    importing auction to route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 import Drops from './Drops'
 import Home from './Home'
+import Auction from './Auction'
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
       <Routes>
       
       <Route exact path="/" element={ <Home /> }/>
+      <Route exact path="/Auction" element={ <Auction /> }/>
       <Route exact path="/Drops" element={ <Drops /> }/>
 
      </Routes>
