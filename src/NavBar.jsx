c0b1e040c5539acd21f28e398eae05066fdcf614
Ken <kendo302003@gmail.com>
2023-05-16 07:34:40 +0100

    linking auction

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
         <ul className="flex flex-row gap-x-5 font-rale">
           <li><a href="#">Home</a></li>
           <li><a href="#">Marketplace</a></li>
-          <li><a href="#">Auctions</a></li>
+          <li><a href="/Auction">Auctions</a></li>
           <li><a href='/Drops'>Drop</a></li>
         </ul>
        <div className="flex flex-row gap-x-5 items-center">
