14f08455f3a424020899611323c6db203354b554
Ken <kendo302003@gmail.com>
2023-05-16 08:52:29 +0100

    done with auctions

--- src/Auctions/Section2.jsx
+++ src/Auctions/Section2.jsx
@@ -5,15 +5,15 @@ const Section2auction = () => {
     <div className="mx-20">
       <p className="mt-20 font-semibold">Top bids from popular creators</p>
 
-      <div>
-        <div>
+      <div className='flex flex-row gap-x-10 mt-10'>
+        <div className='flex flex-col gap-y-4'>
           <img src={ out } />
           <p className='text-gray-600 font-semibold text-lg'>Creator: <span className='text-black'>Dan Murray</span></p>
           <p className='text-gray-600 font-semibold text-lg'>Date: <span className='text-black'>12/08/22</span></p>
           <p className='text-gray-600 font-semibold text-lg'>Highest bid: <span className='text-black'>0.57 ETH</span></p>
 
 
-          <div className='flex flex-row gap-x-6 items-center'>
+          <div className='flex flex-row gap-x-6 ml-10 items-center'>
             <div>
               <p className='text-gray-600 font-semibold text-lg'>Current bid</p>
               <p className='font-semibold text-lg'>0.987 ETH</p>
@@ -23,6 +23,22 @@ const Section2auction = () => {
           </div>
         </div>
 
+        <div className='flex flex-col gap-y-4'>
+          <img src={ fall } />
+          <p className='text-gray-600 font-semibold text-lg'>Creator: <span className='text-black'>Jacob Banks</span></p>
+          <p className='text-gray-600 font-semibold text-lg'>Date: <span className='text-black'>12/08/22</span></p>
+          <p className='text-gray-600 font-semibold text-lg'>Highest bid: <span className='text-black'>0.34 ETH</span></p>
+
+
+          <div className='flex flex-row gap-x-6 ml-10 items-center'>
+            <div>
+              <p className='text-gray-600 font-semibold text-lg'>Current bid</p>
+              <p className='font-semibold text-lg'>0.99 ETH</p>
+            </div>
+            <button className='border-0 bg-blue-700 text-white
+             text-sm px-6 py-1 '>Place bid</button>
+          </div>
+        </div>
 
 
 
