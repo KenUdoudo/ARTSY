2bd9440e6d170d5a66ba7039e8ce8df22476e034
Ken <kendo302003@gmail.com>
2023-05-16 08:14:37 +0100

    hover

--- src/Auctions/Section1.jsx
+++ src/Auctions/Section1.jsx
@@ -7,20 +7,26 @@ const Section1auction = () => {
       <p className="mt-28 font-semibold">Here's an overview of products actively on auction, explore!</p>
 
       <div className='flex flex-row mt-10'>
-        <div className='static'>
+        <a href="#">
+        <div className='static hover:shadow-2xl'>
         <p className='absolute z-10 border-0 rounded bg-slate-700 bg-opacity-25 text-white text-2xl mt-60 ml-24 px-4 py-1'>6hrs: 40mins: 15s</p>
           <img src={ one }/>
         </div>
+        </a>
 
-        <div className='static'>
+        <a href="#">
+        <div className='static hover:shadow-2xl'>
         <p className='absolute z-10 border-0 rounded bg-slate-700 bg-opacity-25 text-white text-2xl mt-60 ml-24 px-4 py-1'>6hrs: 40mins: 15s</p>
           <img src={ three }/>
         </div>
+        </a>
 
-        <div className='static'>
+        <a href="#">
+        <div className='static hover:shadow-2xl'>
         <p className='absolute z-10 border-0 rounded bg-slate-700 bg-opacity-25 text-white text-2xl mt-60 ml-24 px-4 py-1'>6hrs: 40mins: 15s</p>
           <img src={ two }/>
         </div>
+        </a>
       </div>
     </div>
   );
