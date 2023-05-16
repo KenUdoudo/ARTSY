b9a73e81db27cb204cbde4d3924ff76016c465a7
Ken <kendo302003@gmail.com>
2023-05-16 08:25:19 +0100

    importing out

--- src/Auctions/Section2.jsx
+++ src/Auctions/Section2.jsx
@@ -1,7 +1,15 @@
+import out from './assets/Group 527.png'
+import fall from './assets/Group 528 (1).png'
 const Section2auction = () => {
   return (
     <div className="mx-20">
-      <p className="mt-28 font-semibold">Top bids from popular creators</p>
+      <p className="mt-20 font-semibold">Top bids from popular creators</p>
+
+      <div>
+        <div>
+          <img src={ out } />
+        </div>
+      </div>
     </div>
   );
 }
