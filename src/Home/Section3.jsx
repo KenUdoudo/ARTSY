138d8c3f514f14f30417e81822c73d25c68a0de1
Ken <kendo302003@gmail.com>
2023-05-19 16:45:03 +0100

    resizing and padding

--- src/Home/Section3.jsx
+++ src/Home/Section3.jsx
@@ -7,8 +7,8 @@ const Section3home = () => {
   return (
     <div className="static mt-40">
       <div className="absolute mx-20 z-10">
-        <h3 className="mt-6 text-2xl text-white">See Upcoming Auctions and Exibitions</h3>
-        <hr className="my-4 mx-2" />
+        <h3 className="mt-6 text-2xl md:text-xl text-white">See Upcoming Auctions and Exibitions</h3>
+        <hr className="my-4 md:my-1 mx-2" />
         <div>
 
 
