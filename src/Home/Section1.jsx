fe31d4a374e09b00a8c2a8970da135893c40b5f6
Ken <kendo302003@gmail.com>
2023-05-19 10:23:07 +0100

    font size

--- src/Home/Section1.jsx
+++ src/Home/Section1.jsx
@@ -7,9 +7,9 @@ const Section1home = () => {
   return (
 
     <div className="flex flex-col justify-center items-center gap-y-5 text-center mt-5">
-      <h2 className='text-6xl lg:text-5xl font-bold font-bruno'>Photography is poetry & <br/>beautiful untold stories</h2>
+      <h2 className='text-6xl lg:text-5xl md:text-4xl font-bold font-bruno'>Photography is poetry & <br/>beautiful untold stories</h2>
 
-      <p className='text-lg font-rale'>Flip through more than 10,000 vintage shots, old photographs, historic <br />images and captures seamlessly in one place. Register to get top access.</p>
+      <p className='text-lg md:text-base font-rale'>Flip through more than 10,000 vintage shots, old photographs, historic <br />images and captures seamlessly in one place. Register to get top access.</p>
 
       <div className='flex flex-row justify-between gap-x-2 w-full overflow-hidden'>
         <img src={ CarouselOne } className='w-4/5 lg:w-3/5'/>
