ba9e606036685d7c06e8fc872b30d6ac1173c50a
Ken <kendo302003@gmail.com>
2023-05-22 07:38:46 +0100

    hidden and padding

--- src/Home/Section1.jsx
+++ src/Home/Section1.jsx
@@ -10,15 +10,15 @@ const Section1home = () => {
     <div className="flex flex-col justify-center items-center gap-y-5 text-center mt-5 mx-20 md:mx-0">
       <h2 className='text-2xl lg:text-6xl md:text-4xl sm:text-3xl font-bold font-bruno'>Photography is poetry & <br/>beautiful untold stories</h2>
 
-      <p className='text-sm md:text-lg sm:text-base font-rale'>Flip through more than 10,000 vintage shots, old photographs, historic images and captures seamlessly in one place. Register to get top access.</p>
+      <p className='text-sm md:text-lg md:px-20 sm:text-base font-rale'>Flip through more than 10,000 vintage shots, old photographs, historic images and captures seamlessly in one place. Register to get top access.</p>
 
-      <div className='md:flex flex-row justify-between gap-x-2 w-full overflow-hidden hidden'>
+      <div className='lg:flex flex-row justify-between gap-x-2 w-full overflow-hidden hidden'>
         <img src={ CarouselOne } className='w-4/5 lg:w-3/5'/>
         <img src={ CarouselTwo } className= 'w-4/5 lg:w-3/5 '/>
         <img src={ CarouselThree } className='w-4/5 lg:w-3/5 '/>
         <img src={ CarouselFour } className='w-4/5 lg:w-3/5'/>
       </div>
-      <img src={ mainPic } className='hidden sm:block'/>
+      <img src={ mainPic } className='lg:hidden'/>
     </div>
 
   );
