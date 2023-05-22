1a4286a2f239af46689bbaebf49699b6dcc4df8c
Ken <kendo302003@gmail.com>
2023-05-22 07:35:36 +0100

    margin

--- src/Home/Section1.jsx
+++ src/Home/Section1.jsx
@@ -7,7 +7,7 @@ import mainPic from './assets/Component 13home 1st img.png'
 const Section1home = () => {
   return (
 
-    <div className="flex flex-col justify-center items-center gap-y-5 text-center mt-5 sm:mx-20">
+    <div className="flex flex-col justify-center items-center gap-y-5 text-center mt-5 mx-20 md:mx-0">
       <h2 className='text-2xl lg:text-6xl md:text-4xl sm:text-3xl font-bold font-bruno'>Photography is poetry & <br/>beautiful untold stories</h2>
 
       <p className='text-sm md:text-lg sm:text-base font-rale'>Flip through more than 10,000 vintage shots, old photographs, historic images and captures seamlessly in one place. Register to get top access.</p>
