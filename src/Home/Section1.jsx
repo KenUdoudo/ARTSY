2995ceb91fd95f670e0c1736dd3eb86cd53258d3
KenUdoudo <101623697+KenUdoudo@users.noreply.github.com>
2023-05-23 18:38:23 +0100

    Update Section1.jsx

--- src/Home/Section1.jsx
+++ src/Home/Section1.jsx
@@ -7,7 +7,7 @@ import mainPic from './assets/Component 13home 1st img.png'
 const Section1home = () => {
   return (
 
-    <div className="flex flex-col justify-center items-center gap-y-5 text-center mt-5 mx-20 md:mx-0">
+    <div className="flex flex-col justify-center items-center gap-y-5 text-center mt-5 mx-10 md:mx-0">
       <h2 className='text-2xl lg:text-6xl md:text-4xl sm:text-3xl font-bold font-bruno'>Photography is poetry & <br/>beautiful untold stories</h2>
 
       <p className='text-sm md:text-lg md:px-20 lg:px-40 sm:text-base font-rale'>Flip through more than 10,000 vintage shots, old photographs, historic images and captures seamlessly in one place. Register to get top access.</p>
