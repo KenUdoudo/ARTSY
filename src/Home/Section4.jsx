b7be87a20dcd8fca4b78aa2bf1b73a2bcb8e79b2
Ken <kendo302003@gmail.com>
2023-05-24 10:01:47 +0100

    size

--- src/Home/Section4.jsx
+++ src/Home/Section4.jsx
@@ -5,18 +5,18 @@ const Section4home = () => {
     <div className="static bg-gray-200 mt-20 ">
       <div  className='flex flex-row absolute z-10 gap-x-2 mt-60 right-2'>
         <img src={ girl }/>
-        <div className='flex flex-col items-start'>
+        <div className='hidden md:flex flex-col items-start'>
           <h1 className='font-bold text-4xl font-bruno'>CIRCA</h1>
           <h1 className='font-bold text-8xl line-through font-bruno'>1985</h1>
         </div>
       </div>
 
-      <div className='flex flex-row justify-between mx-20 pt-10 z-0'>
+      <div className='flex flex-row justify-between mx-10 md:mx-20 pt-10 z-0'>
         <h2 className='font-bold text-lg sm:text-xl md:text-3xl font-bruno'>TOP CREATORS OF <br />THE WEEK</h2>
         <div className='flex flex-row gap-x-3'>
           <img src={ loader2 } className='w-auto h-3/6'/>
           <div>
-            <ul className='flex flex-col gap-3 text-lg font-rale'>
+            <ul className='flex flex-col gap-3 text-sm sm:text-base md:text-lg font-rale'>
               <a href="#"><li>Editorials</li></a>
               <a href="#"><li>Fashion</li></a>
               <a href="#"><li>Lifestyle</li></a>
