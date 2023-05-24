4cbe55e053717eea4033044c4a0ccd3ae652ab6f
Ken <kendo302003@gmail.com>
2023-05-24 15:08:57 +0100

    done with drops

--- src/Drops/Section2.jsx
+++ src/Drops/Section2.jsx
@@ -9,7 +9,7 @@ const Section2drops = () => {
         
           <img src={ eyo } className='md:w-3/6 w-5/6'/>
       
-        <div className='flex flex-col md:gap-y-6 sm:gap-y-3 gap-y-1 justify-center items-start'>
+        <div className='flex flex-col md:gap-y-4 sm:gap-y-3 gap-y-1 justify-center items-start'>
           <p className='border-0 rounded px-6 py-1 bg-blue-700 text-white text-sm'>UPCOMING</p>
           <p className='text-sm'>November 21 at 11 am WAT</p>
           <h3 className='text-lg font-semibold'>Eyo : Eko For Show</h3>
@@ -23,7 +23,7 @@ const Section2drops = () => {
         
           <img src={ ginger } className='md:w-3/6 w-5/6'/>
       
-        <div className='flex flex-col md:gap-y-6 sm:gap-y-3 gap-y-1 justify-center items-start'>
+        <div className='flex flex-col md:gap-y-4 sm:gap-y-3 gap-y-1 justify-center items-start'>
           <p className='border-0 rounded px-6 py-1 bg-green-700 text-white text-sm'>LIVE NOW</p>
           <p className='text-sm'>November 21 at 11 am WAT</p>
           <h3 className='text-lg font-semibold'>Ginger Suburbs</h3>
@@ -37,7 +37,7 @@ const Section2drops = () => {
         
           <img src={ water } className='md:w-3/6 w-5/6'/>
       
-        <div className='flex flex-col md:gap-y-6 sm:gap-y-3 gap-y-1 justify-center items-start'>
+        <div className='flex flex-col md:gap-y-4 sm:gap-y-3 gap-y-1 justify-center items-start'>
           <p className='border-0 rounded px-6 py-1 bg-gray-700 text-white text-sm'>ENDED</p>
           <p className='text-sm'>November 21 at 11 am WAT</p>
           <h3 className='text-lg font-semibold'>Sink</h3>
@@ -51,7 +51,7 @@ const Section2drops = () => {
         
         <img src={ material } className='md:w-3/6 w-5/6'/>
     
-      <div className='flex flex-col md:gap-y-6 sm:gap-y-3 gap-y-1 justify-center items-start'>
+      <div className='flex flex-col md:gap-y-4 sm:gap-y-3 gap-y-1 justify-center items-start'>
         <p className='border-0 rounded px-6 py-1 bg-gray-700 text-white text-sm'>ENDED</p>
         <p className='text-sm'>November 21 at 11 am WAT</p>
         <h3 className='text-lg font-semibold'>Warped 99</h3>
