7c819e5dff1baa7ecaeb3933263449768474a527
Ken <kendo302003@gmail.com>
2023-05-24 14:52:59 +0100

    margin

--- src/Drops/Section1.jsx
+++ src/Drops/Section1.jsx
@@ -1,6 +1,6 @@
 const Section1drops = () => {
   return (  
-    <div className="flex flex-col mt-8 mx-20 items-center font-rale">
+    <div className="flex flex-col mt-8 mx-10 md:mx-20 items-center font-rale">
       <h3 className="text-center text-xl font-semibold">Upcoming Drops</h3>
       <p className="text-center mt-4 text-lg">Turn on notifications so that no drops will miss you</p>
       <button className="mt-4 border border-black rounded px-12 py-1 font-semibold">Notify me</button>
