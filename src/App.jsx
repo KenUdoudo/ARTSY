d71bf29a964944ee50c00e2344b8a150437f2ef7
Ken <kendo302003@gmail.com>
2023-05-24 06:54:11 +0100

    margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Drops from './Drops'
 import Home from './Home'
 import Auction from './Auction'
