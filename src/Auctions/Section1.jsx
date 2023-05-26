76078b9e1e4505b180c3ccf6fcb483f7941ebac5
Ken <kendo302003@gmail.com>
2023-05-26 07:22:45 +0100

    flex col

--- src/Auctions/Section1.jsx
+++ src/Auctions/Section1.jsx
@@ -4,9 +4,9 @@ import three from './assets/Rectangle 245.png'
 const Section1auction = () => {
   return (  
     <div className="mx-10 md:mx-20 font-rale">
-      <p className="mt-28 font-semibold">Here's an overview of products actively on auction, explore!</p>
+      <p className="mt-20 md:mt-28 text-lg font-semibold">Here's an overview of products actively on auction, explore!</p>
 
-      <div className='flex flex-row mt-10'>
+      <div className='flex flex-col md:flex-row mt-10'>
         <a href="#">
         <div className='static hover:shadow-2xl'>
         <p className='absolute z-10 border-0 rounded bg-slate-700 bg-opacity-25 text-white text-2xl mt-60 ml-24 px-4 py-1'>6hrs: 40mins: 15s</p>
