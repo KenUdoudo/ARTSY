161eb54e4a1c13f8fd2f2f64d9e64f384bf63672
Ken <kendo302003@gmail.com>
2023-05-27 07:15:36 +0100

    justify

--- src/Auctions/Section1.jsx
+++ src/Auctions/Section1.jsx
@@ -6,10 +6,10 @@ const Section1auction = () => {
     <div className="mx-10 md:mx-20 font-rale">
       <p className="mt-20 md:mt-28 text-lg font-semibold">Here's an overview of products actively on auction, explore!</p>
 
-      <div className='flex flex-col md:flex-row mt-10'>
+      <div className='flex flex-col md:flex-row justify-center items-center mt-10'>
         <a href="#">
         <div className='static flex hover:shadow-2xl w-full'>
-        <p className='absolute z-10 border-0 rounded bg-slate-700 bg-opacity-25 text-white text-2xl mt-3/6 mx-auto px-4 py-1'>6hrs: 40mins: 15s</p>
+        <p className='absolute z-10 border-0 rounded bg-slate-700 bg-opacity-25 text-white text-2xl mt-36 sm:40 mx-auto px-4 py-1'>6hrs: 40mins: 15s</p>
           <img src={ one }/>
         </div>
         </a>
