7d0c1bf803c4900c36358c3a1859c5a7ab6e9bdc
Ken <kendo302003@gmail.com>
2023-05-29 06:57:53 +0100

    import sec 1

--- src/Marketplace.jsx
+++ src/Marketplace.jsx
@@ -1,8 +1,11 @@
+import Section1market from "./Marketplace/Section1";
 import NavBar from "./NavBar";
 const Marketplace = () => {
   return (
     <div>
       <NavBar />
+
+      <Section1market />
 
     </div>
   );
