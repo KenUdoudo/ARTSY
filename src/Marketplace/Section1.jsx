e4165cee21354d4ea00590382caf7f44dfaa8684
Ken <kendo302003@gmail.com>
2023-05-29 07:10:33 +0100

    flex

--- src/Marketplace/Section1.jsx
+++ src/Marketplace/Section1.jsx
@@ -1,9 +1,9 @@
 import downArrow from './assets/Frame 5.png'
 const Section1market = () => {
   return ( 
-    <div>
-      <input type="search" placeholder="Search" className="bg-gray-300 pl-5"/>
-      <div>
+    <div className='flex flex-row justify-center items-center'>
+      <input type="search" placeholder="Search" className="bg-gray-300 pl-5 w-1/5"/>
+      <div className='w-3/5'>
         <p>See 1-6 of 16 results</p>
         <div>
           <p>Sort By</p>
