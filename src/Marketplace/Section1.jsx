6cc2bae58cd3926eb6f4126f8e2de8d157106e21
Ken <kendo302003@gmail.com>
2023-05-29 07:03:07 +0100

    text

--- src/Marketplace/Section1.jsx
+++ src/Marketplace/Section1.jsx
@@ -1,7 +1,13 @@
 const Section1market = () => {
   return ( 
     <div>
-      <input type="search" placeholder="Search" className="bg-grey"/>
+      <input type="search" placeholder="Search" className="bg-gray-300 pl-5"/>
+      <div>
+        <p>See 1-6 of 16 results</p>
+        <div>
+          <p>Sort By</p>
+        </div>
+      </div>
     </div>
    );
 }
