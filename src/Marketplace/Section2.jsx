7dc0a68536eb284d85d3ebdd777fbb9ac56f6f50
Ken <kendo302003@gmail.com>
2023-05-30 07:19:04 +0100

    section2 left sizing

--- src/Marketplace/Section2.jsx
+++ src/Marketplace/Section2.jsx
@@ -3,7 +3,7 @@ const Section2market = () => {
   return (  
     <div className='mx-10'>
       <div className='flex flex-col'>
-        <div className='flex flex-row border-b-2 w-2/6 justify-start items-center'>
+        <div className='flex flex-row border-black border-b-2 w-1/6 justify-start items-center'>
           <img src={ filter } className='w-10'/>
           <p>Filter</p>
         </div>
