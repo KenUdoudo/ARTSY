28199b81e6e3eecf5668a5f8dabdf792f5c44f60
Ken <kendo302003@gmail.com>
2023-05-30 07:09:33 +0100

    filter

--- src/Marketplace/Section2.jsx
+++ src/Marketplace/Section2.jsx
@@ -1,7 +1,21 @@
+import filter from './assets/Frame 34.png'
 const Section2market = () => {
   return (  
     <div>
-      
+      <div>
+        <div>
+          <img src={ filter }/>
+          <p>Filter</p>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
     </div>
   );
 }
