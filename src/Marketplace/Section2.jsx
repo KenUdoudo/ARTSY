52e6d6e0e3d9a169bc7183eaf001baa45012c0fc
Ken <kendo302003@gmail.com>
2023-05-30 07:19:46 +0100

    margin top

--- src/Marketplace/Section2.jsx
+++ src/Marketplace/Section2.jsx
@@ -1,7 +1,7 @@
 import filter from './assets/Frame 34.png'
 const Section2market = () => {
   return (  
-    <div className='mx-10'>
+    <div className='mx-10 mt-5'>
       <div className='flex flex-col'>
         <div className='flex flex-row border-black border-b-2 w-1/6 justify-start items-center'>
           <img src={ filter } className='w-10'/>
