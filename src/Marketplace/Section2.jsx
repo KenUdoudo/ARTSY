988c93b318ab12124435e9c9f86e57ee44f1d88c
Ken <kendo302003@gmail.com>
2023-05-30 07:11:12 +0100

    flex

--- src/Marketplace/Section2.jsx
+++ src/Marketplace/Section2.jsx
@@ -2,8 +2,8 @@ import filter from './assets/Frame 34.png'
 const Section2market = () => {
   return (  
     <div>
-      <div>
-        <div>
+      <div className='flex flex-col'>
+        <div className='flex flex-row'>
           <img src={ filter }/>
           <p>Filter</p>
         </div>
