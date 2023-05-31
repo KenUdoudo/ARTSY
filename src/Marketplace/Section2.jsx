e4cf0bed42e7569a2148862ac8b7f03f5f4eb671
Ken <kendo302003@gmail.com>
2023-05-31 07:04:33 +0100

    flex and width

--- src/Marketplace/Section2.jsx
+++ src/Marketplace/Section2.jsx
@@ -3,14 +3,14 @@ import down from './assets/Frame 6.png'
 const Section2market = () => {
   return (  
     <div className='mx-10 mt-5'>
-      <div className='flex flex-col'>
-        <div className='flex flex-row border-black border-b-2 w-1/6 justify-start items-center'>
+      <div className='flex flex-col w-1/6 '>
+        <div className='flex flex-row border-black border-b-2 justify-start items-center'>
           <img src={ filter } className='w-10'/>
           <p>Filter</p>
         </div>
 
         <div>
-          <div>
+          <div className='flex flex-row justify-between items-center'>
             <p>By Category</p>
             <img src={ down }/>
           </div>
