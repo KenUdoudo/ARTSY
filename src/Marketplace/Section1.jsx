7be7b7265e2a6cddeed1ede181e6efcd54f2d08a
Ken <kendo302003@gmail.com>
2023-06-02 06:55:30 +0100

    width and gap

--- src/Marketplace/Section1.jsx
+++ src/Marketplace/Section1.jsx
@@ -1,9 +1,9 @@
 import downArrow from './assets/Frame 5.png'
 const Section1market = () => {
   return ( 
-    <div className='flex flex-row justify-center items-center gap-x-20 mx-10 mt-10 md:mx-20'>
+    <div className='flex flex-row justify-center items-center md:gap-x-10 lg:gap-x-20 lg:mx-10 mt-10 md:mx-20'>
       <input type="search" placeholder="Search" className="bg-gray-300 pl-5 w-1/5 rounded"/>
-      <div className='w-3/5 flex flex-row justify-between items-center border-0 shadow-2xl px-4 py-2'>
+      <div className='md:w-4/5 lg:w-3/5 flex flex-row justify-between items-center border-0 shadow-2xl px-4 py-2'>
         <p className='font-bold'>See 1-6 of 16 results</p>
         <div className='flex flex-row items-center'>
           <p className='font-bold'>Sort By</p>
