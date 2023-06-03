79d8d7e9d2df6ce89ac2d04c7a7ee657d1702171
Ken <kendo302003@gmail.com>
2023-06-03 07:14:53 +0100

    display hidden

--- src/Marketplace/Section1.jsx
+++ src/Marketplace/Section1.jsx
@@ -2,8 +2,8 @@ import downArrow from './assets/Frame 5.png'
 const Section1market = () => {
   return ( 
     <div className='flex flex-row justify-center items-center md:gap-x-10 lg:gap-x-20 lg:mx-10 mt-10 md:mx-20'>
-      <input type="search" placeholder="Search" className="bg-gray-300 pl-5 w-1/5 rounded"/>
-      <div className='md:w-4/5 lg:w-3/5 flex flex-row justify-between items-center border-0 shadow-2xl px-4 py-2'>
+      <input type="search" placeholder="Search" className="bg-gray-300 pl-5 w-1/5 rounded hidden md:block"/>
+      <div className='md:w-4/5 lg:w-3/5 flex flex-row justify-between items-center border-0 shadow-2xl px-4 py-2 hidden md:block'>
         <p className='font-bold'>See 1-6 of 16 results</p>
         <div className='flex flex-row items-center'>
           <p className='font-bold'>Sort By</p>
