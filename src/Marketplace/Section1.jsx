9b6c710737495828ca74ee5a565495cc97f4a9dd
Ken <kendo302003@gmail.com>
2023-06-03 07:28:15 +0100

    dropdown

--- src/Marketplace/Section1.jsx
+++ src/Marketplace/Section1.jsx
@@ -3,13 +3,28 @@ const Section1market = () => {
   return ( 
     <div className='flex flex-row justify-center items-center md:gap-x-10 lg:gap-x-20 lg:mx-10 mt-10 md:mx-20'>
       <input type="search" placeholder="Search" className="bg-gray-300 pl-5 w-1/5 rounded hidden md:block"/>
-      <div className='md:w-4/5 lg:w-3/5 flex flex-row justify-between items-center border-0 shadow-2xl px-4 py-2 hidden md:block'>
+      <div className='md:w-4/5 lg:w-3/5 md:flex flex-row justify-between items-center border-0 shadow-2xl px-4 py-2 hidden'>
         <p className='font-bold'>See 1-6 of 16 results</p>
         <div className='flex flex-row items-center'>
           <p className='font-bold'>Sort By</p>
           <img src={ downArrow }/>
         </div>
       </div>
+
+      <button className='md:hidden'>
+        <p>Filter</p>
+        <div className='absolute hidden top-full min-full w-max bg-white shadow-md mt-1 rounded'>
+          <ul>
+            <li>Editorials</li>
+            <li>Fashion</li>
+            <li>Optics</li>
+            <li>Art & Museum</li>
+            <li>Nature</li>
+          </ul>
+        </div>
+      </button>
+
+
     </div>
    );
 }
