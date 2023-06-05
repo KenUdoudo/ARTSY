ed13f66d1f26b7d12148b7277074dfe2cd989378
Ken <kendo302003@gmail.com>
2023-06-05 14:21:13 +0100

    adding img

--- src/Philomena.jsx
+++ src/Philomena.jsx
@@ -1,10 +1,14 @@
 import NavBar from "./NavBar";
+import first from './Marketplace/assets/Rectangle 251.png'
 
 const Philomena = () => {
   return (
     <div>
       <NavBar />
 
+      <div>
+        <img src={ first }/>
+      </div>
       
     </div>
   );
