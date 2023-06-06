02c20b7d5b5a8e7f09c9771528c330170ef7f442
Ken <kendo302003@gmail.com>
2023-06-06 15:55:09 +0100

    text

--- src/Philomena.jsx
+++ src/Philomena.jsx
@@ -8,6 +8,10 @@ const Philomena = () => {
 
       <div>
         <img src={ first }/>
+        <div>
+          <p>Philomena</p>
+          <p>$3.90</p>
+        </div>
       </div>
       
     </div>
