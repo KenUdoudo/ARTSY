10554349a8f8defd28d101ccc4282a7cae0cdfa2
Ken <kendo302003@gmail.com>
2023-06-07 14:34:31 +0100

    donoe with text

--- src/Philomena.jsx
+++ src/Philomena.jsx
@@ -6,11 +6,20 @@ const Philomena = () => {
     <div>
       <NavBar />
 
-      <div className="flex flex-col justify-center items-center mt-20 border border-black mx-20">
+      <div className="flex flex-col justify-center items-center mt-20 border border-black mx-20 py-10">
         <img src={ first }/>
         <div className="flex flex-row justify-center items-center gap-x-10 mt-5">
           <p className="text-2xl font-bold">Philomena</p>
           <p className="text-2xl">$3.90</p>
+        </div>
+        <hr />
+        <p className="items-start">Creator</p>
+        <p>Made in italy</p>
+        <p>Total views: 1.7k views</p>
+        <div>
+          <button>-</button>
+          <p>0</p>
+          <button>+</button>
         </div>
       </div>
       
