4a09b6343eeb54f9def163cb28af51d7b46aa275
Ken <kendo302003@gmail.com>
2023-06-08 09:29:16 +0100

    resize and gap

--- src/Philomena.jsx
+++ src/Philomena.jsx
@@ -6,24 +6,26 @@ const Philomena = () => {
     <div>
       <NavBar />
 
-      <div className="flex flex-col justify-center items-start mt-20 border border-black mx-20 px-10 py-10">
+      <div className="flex flex-col justify-center items-start mt-20 border border-black mx-20 px-10 py-10 gap-y-4">
         <div className="flex flex-col justify-center items-center">
-        <img src={ first }/>
-        <div className="flex flex-row justify-center items-center gap-x-10 mt-5">
-          <p className="text-2xl font-bold">Philomena</p>
-          <p className="text-2xl">$3.90</p>
-        </div>
+          <img src={ first }/>
+          <div className="flex flex-row justify-center items-center gap-x-10 mt-5">
+            <p className="text-2xl font-bold">Philomena</p>
+            <p className="text-2xl">$3.90</p>
+          </div>
         </div>
         <hr />
-        <p className="items-start">Creator</p>
-        <p>Made in italy</p>
-        <p>Total views: 1.7k views</p>
-        <div className="flex flex-row gap-x-4 text-2xl">
-          <button>-</button>
-          <p>0</p>
-          <button>+</button>
+        <div className="flex flex-col gap-y-3 text-xl">
+          <p className="items-start">Creator</p>
+          <p>Made in italy</p>
+          <p>Total views: 1.7k views</p>
+          <div className="flex flex-row gap-x-4 text-2xl">
+           <button>-</button>
+           <p>0</p>
+           <button>+</button>
+          </div>
+          <button className="bg-blue-900 text-white text-lg px-4 py-2">Add to Cart</button>
         </div>
-        <button className="bg-blue-900 text-white text-lg px-4 py-2">Add to Cart</button>
       </div>
       
     </div>
